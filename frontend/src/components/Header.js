c2735949a2e68f9a4e99265699020bb7c76fb98f
Simon Hanukaev <semenh@gmail.com>
2021-07-15 12:06:59 +0300

    Added a button in header seen by admins only that allows them to generate 100 items each time

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import agent from '../agent';
 
 const LoggedOutView = props => {
   if (!props.currentUser) {
     return (
-      <ul class="navbar-nav ml-auto">
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
@@ -31,7 +32,7 @@ const LoggedOutView = props => {
 const LoggedInView = props => {
   if (props.currentUser) {
     return (
-      <ul class="navbar-nav ml-auto">
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <Link to="/" className="nav-link">
             Home
@@ -39,16 +40,24 @@ const LoggedInView = props => {
         </li>
 
         {props.currentUser.role === "admin" && (
-          <li className="nav-item">
-            <Link to="/editor" className="nav-link">
-              <i className="ion-compose"></i>&nbsp;New Item
-            </Link>
-          </li>
+          <>
+            <li className="nav-item">
+              <Link to="/editor" className="nav-link">
+                <i className="ion-compose"/>&nbsp;New Item
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <a href="#" className="nav-link" onClick={generateItems}>
+                <i className="ion-compose"/>&nbsp;Generate Items
+              </a>
+            </li>
+          </>
         )}
 
         <li className="nav-item">
           <Link to="/settings" className="nav-link">
-            <i className="ion-gear-a"></i>&nbsp;Settings
+            <i className="ion-gear-a"/>&nbsp;Settings
           </Link>
         </li>
 
@@ -68,6 +77,54 @@ const LoggedInView = props => {
 
   return null;
 };
+
+function generateItems(e) {
+    e.preventDefault();
+    const promises = [];
+    for (let i = 0; i < 100; i++) {
+        const id = makeid(6);
+        const item = {
+            title: `Item ${id}`,
+            description: `Description for item ${id}`,
+            image: getImage(i),
+            tagList: ['news'],
+            slug: `item-${id}`
+        };
+        promises.push(agent.Items.create(item));
+    }
+    Promise.all(promises).then(() => {
+        alert('items created');
+    }).catch((error) => {
+        alert('error creating items, see console for more info');
+        console.error('error creating items, error: ', error);
+    })
+}
+
+function getImage(i) {
+    const images = [
+        'https://images.unsplash.com/photo-1500423079914-b65af272b8db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMzQ0NTA3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1495100793874-7f94aecae1fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMzQ0NTA3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1484420269607-41d8060e9779?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMzQ0NTA3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1494247872528-c25b4623cf0d?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMzQ0NTA3fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1491466424936-e304919aada7?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1490698900541-76d9b74bdcac?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTd8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1460891053196-b9d4d9483d9b?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1473210630848-e12656228f26?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTh8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1428280047984-b1e0c0cfe2b7?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTZ8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1501949997128-2fdb9f6428f1?ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTV8MTM0NDUwN3x8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+    ];
+    return images[i % images.length];
+}
+
+function makeid(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 class Header extends React.Component {
   render() {
